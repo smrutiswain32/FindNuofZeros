93042fde05056ef7b1e471d5475b1c9f8e227fc0
smruti swain <smrutiswain35@outlook.com>
2020-10-31 22:21:16 +0530

    Add project files.

---
+++ FindNuofZeros/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Linq;
+
+namespace FindNuofZeros
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string givenNumber = "23000494002999400";
+            var res = givenNumber.ToCharArray().ToList();
+            Console.WriteLine(res.Where(x => x.Equals('0')).Select(x => x).Count());
+            Console.ReadLine();
+        }
+    }
+}
